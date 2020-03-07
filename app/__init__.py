6684ec780d2884efdee4fced906c691d18a51b5a
geraltderivia86 <mrzeta86@yahoo.it>
2020-03-07 15:02:58 +0100

    upgrade per heroku

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 api = Api(app,
@@ -10,7 +11,8 @@ api = Api(app,
        description='api Bilancio personale',
        endpoint='api')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://amgltnjvlajofo:e3157b0001b9af3ab0bc06c7f9b1a4aae92fad3e4a97eedd964974da967eab7d@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dhei6grndbv7l'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://amgltnjvlajofo:e3157b0001b9af3ab0bc06c7f9b1a4aae92fad3e4a97eedd964974da967eab7d@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dhei6grndbv7l'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
